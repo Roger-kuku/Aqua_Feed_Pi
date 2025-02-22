7ee4a06756bf5a299c184b8761a00a9f993b68d2
Roger-kuku <rogerlee2510@gmail.com>
2025-02-22 00:14:17 +0000

    Create run.cpp

---
+++ Motor_File/run.cpp
@@ -0,0 +1,33 @@
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "gpioevent.h"
+
+class EventPrinter : public GPIOPin::GPIOEventCallbackInterface {
+	virtual void hasEvent(gpiod_line_event& e) override {
+	    switch (e.event_type) {
+	    case GPIOD_LINE_EVENT_RISING_EDGE:
+		printf("Rising!\n");
+		break;
+	    case GPIOD_LINE_EVENT_FALLING_EDGE:
+		printf("Falling\n");
+		break;
+	    default:
+		printf("Unkown event\n");
+	    }
+	}
+};
+
+int main(int argc, char *argv[]) {
+	fprintf(stderr,"Press any key to stop.\n");
+	EventPrinter callbackInterface;
+	GPIOPin gpiopin;
+	gpiopin.registerCallback(&callbackInterface);
+	const int gpioPinNo = 27;
+	gpiopin.start(gpioPinNo);
+	getchar();
+	gpiopin.stop();
+	return 0;
+}
